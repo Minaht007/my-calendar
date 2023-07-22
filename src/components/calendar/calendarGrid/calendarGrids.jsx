b9aae67e856521d6c2265427e2a4ba4a5cdeee33
Minaht007 <talmresaerch@gmail.com>
2023-07-22 16:31:58 +0300

    color grid cell

--- src/components/calendar/calendarGrid/calendarGrids.jsx
+++ src/components/calendar/calendarGrid/calendarGrids.jsx
@@ -5,11 +5,19 @@ const GridWrapper = style.div`
 display: grid;
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(6, 1fr);
+  grid-gap: 1px;
+  background-color: rgb(245, 242, 98);
 `;
 
 const CellWrapper = style.div`
+
 min-width: 148px;
 min-height: 80px;
+background-color: rgb(183, 232, 235);
+color: rgb(237, 55, 67);
+text-align: right;
+padding: 4px;
+border-radius: 4px;
 `;
 
 const CalendarGrid = () => {
