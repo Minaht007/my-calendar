9e1789171d5fa1087039295d3d958ded2b531784
Minaht007 <talmresaerch@gmail.com>
2023-07-22 23:47:15 +0300

    calendar europ version

--- src/components/helper/calendarHelper.jsx
+++ src/components/helper/calendarHelper.jsx
@@ -1,24 +1,31 @@
-import moment from "moment";
+import {
+  startOfMonth,
+  startOfWeek,
+  endOfMonth,
+  endOfWeek,
+  addDays,
+  format,
+} from "date-fns";
 
 const getCalendarDates = () => {
   const options = {
     year: "numeric",
     month: "numeric",
     day: "numeric",
-    timezone: "UTC",
+    timeZone: "UTC",
   };
 
-  const startDate = moment().startOf("month").startOf("week");
-  const endDate = moment().endOf("month").endOf("week");
+  const startDate = startOfWeek(startOfMonth(new Date()), { weekStartsOn: 1 }); // Начало недели - понедельник
+  const endDate = endOfWeek(endOfMonth(new Date()), { weekStartsOn: 1 }); // Конец недели - понедельник
 
-  const formatDate = (date) => date.format("YYYY-MM-DD");
+  const formatDate = (date) => format(date, "yyyy-MM-dd");
 
   const calendar = [];
 
-  let currentDay = startDate.clone();
-  while (currentDay.isSameOrBefore(endDate)) {
+  let currentDay = startDate;
+  while (currentDay <= endDate) {
     calendar.push({ date: formatDate(currentDay) });
-    currentDay.add(1, "day");
+    currentDay = addDays(currentDay, 1);
   }
 
   return calendar;
