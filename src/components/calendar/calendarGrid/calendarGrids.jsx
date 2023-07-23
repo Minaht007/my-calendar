323b18aba2af79eb5a1c2a0cf94980eccbf86e5a
Minaht007 <talmresaerch@gmail.com>
2023-07-23 15:12:50 +0300

    123

--- src/components/calendar/calendarGrid/calendarGrids.jsx
+++ src/components/calendar/calendarGrid/calendarGrids.jsx
@@ -4,7 +4,7 @@ import { isWeekend } from "date-fns";
 
 const GridWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr) repeat(2, 1fr); // Изменяем порядок столбцов
+  grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(6, 1fr);
   grid-gap: 1px;
   background-color: rgb(245, 242, 98);
@@ -24,7 +24,7 @@ const CellWrapper = styled.div`
 const RowInCell = styled.div`
   display: flex;
   justify-content: ${(props) =>
-    props.justifyContent ? props.justifyContent : "flex-start"};
+    props.justifycontent ? props.justifycontent : "flex-start"};
 `;
 
 const DayWrapper = styled.div`
