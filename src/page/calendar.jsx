918155758ade381e02752d080bd169a17ca4c126
Minaht007 <talmresaerch@gmail.com>
2023-07-30 15:58:07 +0300

    1

--- src/page/calendar.jsx
+++ src/page/calendar.jsx
@@ -2,14 +2,23 @@ import React, { useContext, useEffect } from "react";
 import CalendarGrid from "../components/calendar/calendarGrid/calendarGrids";
 import { getCalendarDates } from "../components/helper/calendarHelper.jsx";
 import { CalendarContext } from "../components/context/contextWrapper";
+import { sub, add } from "date-fns";
 
 const Calendar = () => {
   const { calendar, setCalendar, currentDay, currentMonth, setCurrentMonth } =
     useContext(CalendarContext);
 
   useEffect(() => {
-    const temp = getCalendarDates(currentMonth); // Используйте currentMonth для обновления календаря
-    setCalendar(temp);
+    const currentMonthCalendar = getCalendarDates(currentMonth);
+    const prevMonthCalendar = getCalendarDates(sub(currentMonth, 1));
+    const nextMonthCalendar = getCalendarDates(add(currentMonth, 1));
+
+    const updatedCalendar = [
+      ...prevMonthCalendar,
+      ...currentMonthCalendar,
+      ...nextMonthCalendar,
+    ];
+    setCalendar(updatedCalendar);
   }, [currentMonth, setCalendar]);
 
   return (
