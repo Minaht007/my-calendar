4d0d7303ae54c9e3f593a2199749ab4aa0dc4814
Minaht007 <talmresaerch@gmail.com>
2023-08-05 20:41:01 +0300

    viewer

--- src/components/helper/calendarWeek.jsx
+++ src/components/helper/calendarWeek.jsx
@@ -1,36 +1,43 @@
 import React, { useContext } from "react";
 import { CalendarContext } from "../context/contextWrapper";
-import { format, startOfWeek, addDays } from "date-fns";
+import { format, startOfDay, addDays, isSameDay } from "date-fns";
 
 const CalendarWeek = () => {
-  const { currentMonth, currentYear } = useContext(CalendarContext);
+  const { currentDay } = useContext(CalendarContext);
+  const today = new Date();
 
   const getNearestDates = () => {
-    const firstDayOfWeek = startOfWeek(currentMonth);
-    const nearestDates = [firstDayOfWeek];
+    const firstDayOfWeek = addDays(startOfDay(currentDay), -2);
+    const nearestDates = [];
 
-    for (let i = 1; i < 7; i++) {
-      nearestDates.push(addDays(firstDayOfWeek, i));
+    for (let i = 0; i < 7; i++) {
+      const date = addDays(firstDayOfWeek, i);
+      nearestDates.push(date);
     }
 
     return nearestDates;
   };
 
   return (
-    <div className=" w-full h-full px-2 py-3 ">
-      <h4 className=" text-red-500 font-bold text-xl">Calendar Week</h4>
-      <div className=" flex w-full h-full overflow-y-scroll">
-        <div className=" grid grid-rows-25 w-[50px]">
-          <div className=" w-[50px] h-[30px]"></div>
+    <div className="w-full h-full px-2 py-3">
+      <h4 className="text-red-500 font-bold text-xl">Calendar Week</h4>
+      <button>prev</button>
+      <button>next</button>
+
+      <div className="flex w-full h-full overflow-y-scroll">
+        <div className="grid grid-rows-25 w-[50px]">
+          <div className="w-[50px] h-[30px]"></div>
           {Array.from({ length: 24 }).map((_, index) => (
-            <div key={index} className=" flex h-[30px] items-center">
+            <div key={index} className="flex h-[30px] items-center">
               <p>{index + 1}</p>
             </div>
           ))}
         </div>
-        <div className=" flex flex-row w-full h-full">
+        <div className="flex flex-row w-full h-full">
           {getNearestDates().map((date, indexData) => {
             const formattedDate = format(date, "d");
+            const isTodayDate = isSameDay(date, today);
+
             return (
               <div key={date}>
                 <div
@@ -38,13 +45,13 @@ const CalendarWeek = () => {
                     indexData === 6 ? "lg:grid-rows-10" : "lg:grid-rows-6"
                   } w-[30px]`}
                 >
-                  <p className="h-[30px]">{formattedDate}</p>
+                  <p className={`h-[30px]`}>{formattedDate}</p>
                   {Array.from({ length: 24 }).map((_, indexTime) => (
                     <div
                       key={indexTime}
                       className="flex h-[30px] items-center border-b-[1px] border-r-[1px] border-gray-200"
                       onClick={() => {
-                        alert(date + " time " + indexTime);
+                        alert(formattedDate + " time " + indexTime);
                       }}
                       style={{
                         borderBottom:
