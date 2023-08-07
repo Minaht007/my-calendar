da2d75b323a313030fd46b65a40718ddf05d7f87
Minaht007 <talmresaerch@gmail.com>
2023-08-07 15:16:58 +0300

    12345

--- src/components/helper/calendarWeekNavigate.jsx
+++ src/components/helper/calendarWeekNavigate.jsx
@@ -1,11 +1,10 @@
 import React, { useContext, useState, useEffect } from "react";
-import { subDays, startOfDay, addDays } from "date-fns";
+import { subDays, startOfDay, addDays, previousDay } from "date-fns";
 import { CalendarContext } from "../context/contextWrapper";
 
 const WeekNavigate = ({ prevWeek, nextWeek }) => {
   const [currentDay] = useContext(CalendarContext);
-  //   const [calendarWeek, setCalendarWeek] = useState(new Date());
-  const [previousDate, setPrevDays] = useState();
+  const [calendarWeek, setCalendarWeek] = useState(new Date());
 
   //   useEffect(() => {
   //     const todayWeek = calendarWeek.getDate();
@@ -15,24 +14,24 @@ const WeekNavigate = ({ prevWeek, nextWeek }) => {
   console.log("1");
   const handlePreviousWeek = () => {
     debugger;
-    const today = currentDay;
-    const previousDate = [];
-    console.log(previousDate);
+    const previousDates = [];
+    console.log(previousDates);
     console.log("2");
     for (let i = 1; i <= 7; i++) {
-      previousDate = subDays(today, i);
-      previousDate.push(startOfDay(previousDate));
+      const previousDate = subDays(currentDay, i);
+      previousDates.push(startOfDay(previousDate));
     }
-    return prevWeek(nextDays[0]);
+    return prevWeek(previousDates[0]);
   };
 
   const handleNextWeek = () => {
     const nextDays = [];
+    console.log("3");
     for (let i = 1; i <= 7; i++) {
-      nextDate = addDays(currentDay, i);
+      const nextDate = addDays(currentDay, i);
       nextDays.push(startOfDay(nextDate));
     }
-    nextWeek(nextDays[0]);
+    return nextWeek(nextDays[0]);
   };
   console.log(handleNextWeek());
   return (
