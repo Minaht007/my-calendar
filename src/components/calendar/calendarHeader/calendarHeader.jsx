575c4fc8fcaa3dc648424d40ac04195a7fc80c79
Minaht007 <talmresaerch@gmail.com>
2023-08-09 01:27:49 +0300

    style

--- src/components/calendar/calendarHeader/calendarHeader.jsx
+++ src/components/calendar/calendarHeader/calendarHeader.jsx
@@ -21,8 +21,7 @@ const CalendarHeader = ({ onChangeView }) => {
   };
 
   return (
-    <DivWrapper>
-      Title
+    <DivWrapper className="flex justify-around items-center pl-4 pr-4">
       <Link to="/">Month</Link>
       <Link to="/week">Week</Link>
       <Link to="/day">Day</Link>
