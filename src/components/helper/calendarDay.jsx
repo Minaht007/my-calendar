2a2775fb3884411980ed363a22c3f8da3118fa03
Minaht007 <talmresaerch@gmail.com>
2023-08-09 11:31:39 +0300

    border Day

--- src/components/helper/calendarDay.jsx
+++ src/components/helper/calendarDay.jsx
@@ -11,9 +11,12 @@ const CalendarDay = () => {
       <h3>Calendar Day</h3>
       <div>
         <p>{format(currentDay, "eeee, MMMM dd")}</p>
-        <div>
+        <div mt-10>
           {Array.from({ length: 24 }).map((_, index) => (
-            <div key={index} className="flex h-[30px] items-center">
+            <div
+              key={index}
+              className="flex h-[30px] items-center  border-b-2 border-red-200"
+            >
               <p>{index + 1}</p>
             </div>
           ))}
